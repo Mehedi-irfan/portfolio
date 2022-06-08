9353b898412f6daca7bc4f94bdb78d3c4870bc46
Mehedi Irfan <mehediirfan99@gmail.com>
2022-06-08 20:30:24 +0600

    experianced component added

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './About.css';
 import AboutME from '../../asstes/images/20191106091849_IMG_1222-removebg-preview.png';
 import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc';
 
 const About = () => {
     return (
@@ -24,19 +26,19 @@ const About = () => {
                         </article>
 
                         <article className='about__card'>
-                            <FaAward className='about__icon' />
+                            <FiUsers className='about__icon' />
                             <h5>Clients</h5>
                             <small>5 Clients Handle</small>
                         </article>
 
                         <article className='about__card'>
-                            <FaAward className='about__icon' />
+                            <VscFolderLibrary className='about__icon' />
                             <h5>Projects</h5>
                             <small>10 Projects</small>
                         </article>
 
                     </div>
-                    <p>I am a Front End developer based in Bangladesh. I have developed multiple MERN stack projects and 10+ Front End projects. Programming is my passion. I love to code. I devote my full attention to designing and building websites. My biggest strength is I never stop learning and never give up when getting any Bugs / Errors on the code. Rather than spending hours trying to figure it out. I try several methods. Eventually, I found and fixed the problem.</p>
+                    <p>I am a Front End developer based in Bangladesh. I have developed multiple MERN stack projects and 10+ Front End projects. Programming is my passion. I devote my full attention to designing and building websites. My biggest strength is I never stop learning and never give up when getting any Bugs / Errors on the code. Rather than spending hours trying to figure it out. I try several methods. Eventually, I found and fixed the problem.</p>
 
                     <a href="#contact" className='btn btn-primary'>Let's Talk</a>
                 </div>
--- src/Component/Experiance/Experiance.jsx
+++ src/Component/Experiance/Experiance.jsx
@@ -1,10 +1,97 @@
 import React from 'react';
-import './Experiance.css'
+import './Experiance.css';
+import { BsPatchCheckFill } from 'react-icons/bs';
 
 const Experiance = () => {
     return (
         <section id='experiance'>
-            <h1>Experiance</h1>
+            <h5>What Skills I Have</h5>
+            <h2>My Experiance</h2>
+
+            <div className="container experiance__container">
+                <div className="experiance__frontend">
+                    <h3>Front End Development</h3>
+                    <div className="experiance__content">
+                        <article className='experiance__details'>
+                            <BsPatchCheckFill className='experiance__details__icon' />
+                            <div>
+                                <h4>HTML</h4>
+                                <small className='text-light'>Experianced</small>
+                            </div>
+                        </article>
+
+                        <article className='experiance__details'>
+                            <BsPatchCheckFill className='experiance__details__icon' />
+                            <div>
+                                <h4>CSS</h4>
+                                <small className='text-light'>Experianced</small>
+                            </div>
+                        </article>
+
+                        <article className='experiance__details'>
+                            <BsPatchCheckFill className='experiance__details__icon' />
+                            <div>
+                                <h4>BOOTSTRAP</h4>
+                                <small className='text-light'>Experianced</small>
+                            </div>
+                        </article>
+
+                        <article className='experiance__details'>
+                            <BsPatchCheckFill className='experiance__details__icon' />
+                            <div>
+                                <h4>JAVASCRIPT</h4>
+                                <small className='text-light'>Intermediate</small>
+                            </div>
+                        </article>
+
+                        <article className='experiance__details'>
+                            <BsPatchCheckFill className='experiance__details__icon' />
+                            <div>
+                                <h4>TAILWIND</h4>
+                                <small className='text-light'>Intermediate</small>
+                            </div>
+                        </article>
+
+                        <article className='experiance__details'>
+                            <BsPatchCheckFill className='experiance__details__icon' />
+                            <div>
+                                <h4>React Js</h4>
+                                <small className='text-light'>Intermediate</small>
+                            </div>
+                        </article>
+
+                    </div>
+                </div>
+                <div className="experiance__backend">
+                    <h3>Back End Development</h3>
+                    <div className="experiance__content">
+
+                        <article className='experiance__details'>
+                            <BsPatchCheckFill className='experiance__details__icon' />
+                            <div>
+                                <h4>NODE JS</h4>
+                                <small className='text-light'>Basic</small>
+                            </div>
+                        </article>
+
+                        <article className='experiance__details'>
+                            <BsPatchCheckFill className='experiance__details__icon' />
+                            <div>
+                                <h4>EXPRESS JS</h4>
+                                <small className='text-light'>Basic</small>
+                            </div>
+                        </article>
+
+                        <article className='experiance__details'>
+                            <BsPatchCheckFill className='experiance__details__icon' />
+                            <div>
+                                <h4>MONGODB</h4>
+                                <small className='text-light'>Basic</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
