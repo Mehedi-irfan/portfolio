1d7e7d1952600d5c2cc91cf57acb6bd0cd29e436
Mehedi Irfan <mehediirfan99@gmail.com>
2022-06-11 23:04:50 +0600

    footer componented added

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import './Footer.css';
+import { FaFacebookF } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
+import { IoLogoTwitter } from 'react-icons/io'
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer>
+            <a href="/" className='footer__logo'>Mehedi Irfan</a>
+
+            <ul className='permalinks'>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experiance">Experiance</a></li>
+                <li><a href="#services">Service</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#testimonial">Testimonial</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className="footer__scails">
+                <a href="/"><FaFacebookF /></a>
+                <a href="/"><FiInstagram /></a>
+                <a href="/"><IoLogoTwitter /></a>
+            </div>
+
+            <div className="footer__copyright">
+                <small>&copy; Mehedi Irfan, All rights Reserved</small>
+            </div>
+        </footer>
     );
 };
 
