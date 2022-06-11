abec8279bb0b375a4c4cf409e6288368cc13136b
Mehedi Irfan <mehediirfan99@gmail.com>
2022-06-11 22:03:30 +0600

    email js added

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -3,9 +3,19 @@ import './Contact.css';
 import { MdOutlineEmail } from 'react-icons/md';
 import { RiMessengerLine } from 'react-icons/ri';
 import { BsWhatsapp } from 'react-icons/bs';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_v6113au', 'template_u2ullhc', form.current, 'user_8l20h8WITtZ02Yx20Q36j');
+        e.target.reset();
+    };
     return (
         <section id='contact'>
             <h5>Get In Touch</h5>
@@ -33,11 +43,11 @@ const Contact = () => {
                     </article>
                 </div>
                 {/* END OF CONTACT OPTION  */}
-                <form action="">
+                <form ref={form} onSubmit={sendEmail}>
                     <input type="text" name='name' placeholder='Enter Your Full Name' required />
                     <input type="email" name="email" placeholder='Enter Your Email' id="" required />
                     <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-                    <button className='btn btn-primary' type='submit'>Submit</button>
+                    <button className='btn btn-primary' type='submit'>Send Message</button>
                 </form>
             </div>
         </section>
